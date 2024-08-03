3d2b7e0cfe21844f929223efcc86bc6e1244c19a
Gavin Morgan <morgan.gavin@gmail.com>
2024-08-03 12:34:49 -0700

    Adds "about" text

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import {h} from "preact"
+import {h, Fragment} from "preact"
 import "./app.css"
 
 export function App() {
-    return <h1>Hello from Preact!</h1>
+    return <>
+        <h1>EchoDelphi!        </h1>
+        <p>EchoDelphi is an application that summarizes a spoken language audio stream and makes a printed summary of it. 
+            As an example, the audio of a meeting can be transcribed in realtime, summarized and displayed on a monitor for the meeting participants to see a summary of their meeting so far. That way they can address missi items they still need to discuss.
+            
+        </p>
+        <button>Start</button>
+    </>
 }
 
-test("App", {
-    "renders a greeting"() {
-        expect(App().props.children, equals, "Hello from Preact!")
-    },
-})
