ff191b4c30af79fd2beb206e57ff85de2587f132
Nyan Soe <nyanhpsoe@gmail.com>
2024-08-03 14:44:02 -0700

    Linting

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,18 @@ import "./app.css"
 import "./voice"
 
 export function App() {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0)
 
-    return <>
-        <h1>EchoDelphi!        </h1>
-        <button onClick={() => setCount(count+1)}>Start</button>
-        <p>Count: {count}</p>
-        <p>EchoDelphi is an application that summarizes a spoken language audio stream and makes a printed summary of it. 
+    return (
+        <>
+            <h1>EchoDelphi!        </h1>
+            <button onClick={() => setCount(count + 1)}>Start</button>
+            <p>Count: {count}</p>
+            <p>EchoDelphi is an application that summarizes a spoken language audio stream and makes a printed summary of it.
         As an example, the audio of a meeting can be transcribed in realtime, summarized and displayed on a monitor for the meeting participants to see a summary of their meeting so far. That way they can address missi items they still need to discuss.
-        </p>
+            </p>
 
-    </>
+        </>
+    )
 }
 
